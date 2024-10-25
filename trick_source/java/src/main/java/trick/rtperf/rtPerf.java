f015bc26339432662f0ea998867c59c875d35066
AdityaGirish <aditya.girish@nasa.gov>
2024-10-25 09:42:02 -0500

    Added JobExecutionEvent class.

--- trick_source/java/src/main/java/trick/rtperf/rtPerf.java
+++ trick_source/java/src/main/java/trick/rtperf/rtPerf.java
@@ -9,3 +9,23 @@ import java.util.*;
 import java.util.List;
 import javax.swing.*;
 import javax.swing.event.*;
+
+class JobExecutionEvent {
+    public String id;
+    public boolean isEOF;
+    public boolean isTOF;
+    public double start;
+    public double stop;
+
+    public JobExecutionEvent(String identifier, boolean isTopOfFrame, boolean isEndOfFrame, double start_time, double stop_time) {
+        id = identifier;
+        isEOF = isEndOfFrame;
+        isTOF = isTopOfFrame;
+        start = start_time;
+        stop = stop_time;
+    }
+
+    public String toString() {
+        return ( "JobExecutionEvent: " + id + "," + start + "," + stop );
+    }
+}
